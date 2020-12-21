78749dd92de189fa84414f02ea1c6b0c0887a867
victorino <giovanna.victorino@gmail.com>
2020-12-21 20:20:53 -0300

    Mocking matches for tv

--- tv/src/main/java/com/devcamp/tv/ui/main/MainActivity.kt
+++ tv/src/main/java/com/devcamp/tv/ui/main/MainActivity.kt
@@ -101,16 +101,91 @@ class MainActivity : AppCompatActivity(), View.OnFocusChangeListener, View.OnCli
 
     private fun setupMatchRecyclerAdapter() {
         val matches = listOf(
-            Match(homeTeam = "FLA", homeScore = 1, visitorTeam = "VAS", visitorScore = 0),
-            Match(homeTeam = "FLA", homeScore = 1, visitorTeam = "VAS", visitorScore = 0),
-            Match(homeTeam = "FLA", homeScore = 1, visitorTeam = "VAS", visitorScore = 0),
-            Match(homeTeam = "FLA", homeScore = 1, visitorTeam = "VAS", visitorScore = 0),
-            Match(homeTeam = "FLA", homeScore = 1, visitorTeam = "VAS", visitorScore = 0),
-            Match(homeTeam = "FLA", homeScore = 1, visitorTeam = "VAS", visitorScore = 0),
-            Match(homeTeam = "FLA", homeScore = 1, visitorTeam = "VAS", visitorScore = 0),
-            Match(homeTeam = "FLA", homeScore = 1, visitorTeam = "VAS", visitorScore = 0),
-            Match(homeTeam = "FLA", homeScore = 1, visitorTeam = "VAS", visitorScore = 0),
-            Match(homeTeam = "FLA", homeScore = 1, visitorTeam = "VAS", visitorScore = 0)
+            Match(
+                homeTeam = "CAM",
+                homeScore = 2,
+                homeDrawable = R.drawable.ic_cam,
+                visitorTeam = "BOT",
+                visitorScore = 1,
+                visitorDrawable = R.drawable.ic_bot,
+                videoDrawable = R.raw.acg_int
+            ),
+            Match(
+                homeTeam = "ACG",
+                homeScore = 0,
+                homeDrawable = R.drawable.ic_acg,
+                visitorTeam = "INT",
+                visitorScore = 0,
+                visitorDrawable = R.drawable.ic_int,
+                videoDrawable = R.raw.acg_int
+            ),
+            Match(
+                homeTeam = "BAH",
+                homeScore = 1,
+                homeDrawable = R.drawable.ic_bah,
+                visitorTeam = "SAO",
+                visitorScore = 3,
+                visitorDrawable = R.drawable.ic_sao,
+                videoDrawable = R.raw.acg_int
+            ),
+            Match(
+                homeTeam = "CFC",
+                homeScore = 0,
+                homeDrawable = R.drawable.ic_cfc,
+                visitorTeam = "COR",
+                visitorScore = 1,
+                visitorDrawable = R.drawable.ic_cor,
+                videoDrawable = R.raw.acg_int
+            ),
+            Match(
+                homeTeam = "FLU",
+                homeScore = 0,
+                homeDrawable = R.drawable.ic_flu,
+                visitorTeam = "BGT",
+                visitorScore = 0,
+                visitorDrawable = R.drawable.ic_bgt,
+                videoDrawable = R.raw.acg_int
+            ),
+
+            Match(
+                homeTeam = "FOR",
+                homeScore = 1,
+                homeDrawable = R.drawable.ic_for,
+                visitorTeam = "GOI",
+                visitorScore = 1,
+                visitorDrawable = R.drawable.ic_goi,
+                videoDrawable = R.raw.acg_int
+            ),
+
+            Match(
+                homeTeam = "PAL",
+                homeScore = 3,
+                homeDrawable = R.drawable.ic_pal,
+                visitorTeam = "ATL",
+                visitorScore = 0,
+                visitorDrawable = R.drawable.ic_atl,
+                videoDrawable = R.raw.acg_int
+            ),
+
+            Match(
+                homeTeam = "SAN",
+                homeScore = 1,
+                homeDrawable = R.drawable.ic_san,
+                visitorTeam = "SPT",
+                visitorScore = 1,
+                visitorDrawable = R.drawable.ic_spt,
+                videoDrawable = R.raw.acg_int
+            ),
+
+            Match(
+                homeTeam = "VAS",
+                homeScore = 1,
+                homeDrawable = R.drawable.ic_vas,
+                visitorTeam = "CEA",
+                visitorScore = 4,
+                visitorDrawable = R.drawable.ic_cea,
+                videoDrawable = R.raw.acg_int
+            )
         )
         binding.matchesRecyclerView.adapter = MatchRecyclerAdapter(matches).apply {
             onItemClickListener = {
--- tv/src/main/java/com/devcamp/tv/ui/main/model/Match.kt
+++ tv/src/main/java/com/devcamp/tv/ui/main/model/Match.kt
@@ -3,6 +3,9 @@ package com.devcamp.tv.ui.main.model
 data class Match(
     val homeTeam : String,
     val homeScore : Int,
+    val homeDrawable : Int,
     val visitorTeam : String,
-    val visitorScore : Int
+    val visitorScore : Int,
+    val visitorDrawable : Int,
+    val videoDrawable : Int
 )
