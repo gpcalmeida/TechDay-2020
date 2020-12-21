37856f76b0182d7891852f888a292238bbe17ec3
EmilioHoffmann <emilio.hoffmann@4all.com>
2020-12-21 15:22:08 -0300

    Iniciando player

--- tv/src/main/java/com/devcamp/tv/FragmentActivityExtensions.kt
+++ tv/src/main/java/com/devcamp/tv/FragmentActivityExtensions.kt
@@ -13,14 +13,14 @@ const val ROOT_TAG = "root_fragment"
  * Adds a fragment to the master back stack, allowing retrieval using tag. Should be used for
  * top-level destinations from the bottom navigation view.
  */
-fun FragmentActivity.onDestinationSelected(fragmentTag: String) {
-    supportFragmentManager.popBackStack(ROOT_TAG, 0)
-    supportFragmentManager.beginTransaction()
-            .setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out)
-            .replace(R.id.activity_main_container, findSelectedFragment(fragmentTag), fragmentTag)
-            .addToBackStack(ROOT_TAG)
-            .commitAllowingStateLoss()
-}
+//fun FragmentActivity.onDestinationSelected(fragmentTag: String) {
+//    supportFragmentManager.popBackStack(ROOT_TAG, 0)
+//    supportFragmentManager.beginTransaction()
+//            .setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out)
+//            .replace(R.id.activity_main_container, findSelectedFragment(fragmentTag), fragmentTag)
+//            .addToBackStack(ROOT_TAG)
+//            .commitAllowingStateLoss()
+//}
 
 fun FragmentActivity.findSelectedFragment(fragmentTag: String): Fragment {
     return supportFragmentManager.findFragmentByTag(fragmentTag).let {
--- tv/src/main/java/com/devcamp/tv/ui/main/MainActivity.kt
+++ tv/src/main/java/com/devcamp/tv/ui/main/MainActivity.kt
@@ -1,22 +1,19 @@
 package com.devcamp.tv.ui.main
 
 import android.os.Bundle
-import android.transition.TransitionManager
-import android.view.KeyEvent
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.content.ContextCompat
 import com.devcamp.tv.*
-import com.devcamp.tv.ui.account.AccountFragment
-import com.devcamp.tv.ui.account.AccountFragment.Companion.ACCOUNT_TAG
-import com.devcamp.tv.ui.home.HomeFragment
-import com.devcamp.tv.ui.home.HomeFragment.Companion.HOME_TAG
+import com.devcamp.tv.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnFocusChangeListener,View.OnClickListener {
     private var isFirstTime: Boolean = true
+    lateinit var binding: ActivityMainBinding
 
     init {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
@@ -24,117 +21,41 @@ class MainActivity : AppCompatActivity(), View.OnFocusChangeListener,View.OnClic
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
 
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
 
-        unselectedViews(activity_main_text_view_account)
+    fun setExoPlayer(){
+        val exoPlayer = SimpleExoPlayer.Builder(this).build()
+        with(exoPlayer) {
 
-        activity_main_text_view_home.setOnClickListener(this)
-        activity_main_text_view_account.setOnClickListener(this)
+            val mediaItem = MediaItem.Builder()
+                .setUri(getVideoResourcePath(R.raw.acg_int))
+                .build()
 
-        activity_main_text_view_home.onFocusChangeListener = this
-        activity_main_text_view_account.onFocusChangeListener = this
+            this.addMediaItem(mediaItem)
+            this.prepare()
+            this.play()
 
-        activity_main_text_view_home.selected()
-        activity_main_text_view_home.requestFocus()
-        onDestinationSelected(HOME_TAG)
-
-        openDrawer()    }
-
-    override fun onBackPressed() {
-        val viewSelected = whichViewIsSelected(activity_main_text_view_home, activity_main_text_view_account)
-
-        if (!drawerIsOpen()) {
-            openDrawer()
-
-            when (viewSelected) {
-                R.id.activity_main_text_view_home -> {
-                    (supportFragmentManager.findFragmentByTag(HOME_TAG) as? HomeFragment)?.drawerOpen()
-                    activity_main_text_view_home.focusDelayed()
-                }
-
-                R.id.activity_main_text_view_account -> {
-                    (supportFragmentManager.findFragmentByTag(ACCOUNT_TAG) as? AccountFragment)
-                    activity_main_text_view_home.focusDelayed()
-                }
-            }
-            return
-        }
-
-        closeDrawer()
-        when (viewSelected) {
-            R.id.activity_main_text_view_home -> {
-                activity_main_text_view_home.clearFocus()
-                (supportFragmentManager.findFragmentByTag(HOME_TAG) as? HomeFragment)?.drawerClosed()
-            }
-
-            R.id.activity_main_text_view_account -> {
-                activity_main_text_view_account.clearFocus()
-                (supportFragmentManager.findFragmentByTag(ACCOUNT_TAG) as? AccountFragment)
-            }
+            binding.matchPlayer.player = this
         }
     }
 
-    private fun openDrawer() {
-        activity_main_content_menu.layoutParams = FrameLayout.LayoutParams(resources.getDimensionPixelSize(R.dimen.spacings_two_hundred_fifty), FrameLayout.LayoutParams.MATCH_PARENT)
-        activity_main_container.foreground = ContextCompat.getDrawable(baseContext,
-            R.color.blackFortyTransparency
-        )
-        TransitionManager.beginDelayedTransition(activity_main_content_menu)
+    private fun getVideoResourcePath(res: Int): String {
+        return "android.resource://${this.packageName}/${res}"
     }
-
-    private fun closeDrawer() {
-        activity_main_content_menu.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.MATCH_PARENT)
-        activity_main_container.foreground = null
-        TransitionManager.beginDelayedTransition(activity_main_content_menu)
-    }
-
-
-    override fun onKeyLongPress(keyCode: Int, event: KeyEvent?): Boolean {
-        when (event?.keyCode) {
-            KeyEvent.KEYCODE_BACK -> {
-                finish()
-            }
-        }
-        return super.onKeyLongPress(keyCode, event)
-    }
-
-    private fun drawerIsOpen() = activity_main_content_menu.layoutParams.width == resources.getDimensionPixelSize(R.dimen.spacings_two_hundred_fifty)
 
     override fun onFocusChange(view: View?, hasFocus: Boolean) {
         when (view?.id) {
-            R.id.activity_main_text_view_home,
-            R.id.activity_main_text_view_account -> {
-                if (!isFirstTime) {
-                    if (hasFocus) {
-                        openDrawer()
-                        return
-                    }
-
-                    closeDrawer()
-                } else {
-                    isFirstTime = false
-                }
-            }
         }
     }
 
     override fun onClick(view: View?) {
         view?.run {
             when (id) {
-                R.id.activity_main_text_view_home -> {
-                    unselectedViews(activity_main_text_view_account)
-                    selected()
-                    onDestinationSelected(HOME_TAG)
-                }
 
-                R.id.activity_main_text_view_account -> {
-                    unselectedViews(activity_main_text_view_home)
-                    selected()
-                    onDestinationSelected(ACCOUNT_TAG)
-                }
             }
-            closeDrawer()
         }
     }
 }
