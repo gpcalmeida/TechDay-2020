0b73837d2b7a4415f295b3f506b6465eb766281a
gustavo monteiro <gustavo.monteiro@dextra-sw.com>
2020-12-22 17:29:27 -0300

    Add assets (videos and logos)

---
+++ data/src/main/java/network/Info.kt
@@ -0,0 +1,3 @@
+package network
+
+data class Info(val title: String, val content: String)
--- data/src/main/java/network/Match.kt
+++ data/src/main/java/network/Match.kt
@@ -4,5 +4,6 @@ data class Match(
     val home: String,
     val away: String,
     val score: String,
-    val tournament: String
+    val tournament: String,
+    val info: Info?
 )
