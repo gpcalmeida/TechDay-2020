468f6f8ef504113101a0e6d4428363641175d7d7
victorino <giovanna.victorino@gmail.com>
2020-12-22 17:23:33 -0300

    item info position at main fragment

--- app/src/main/java/com/techday2020/ui/main/view/MainFragment.kt
+++ app/src/main/java/com/techday2020/ui/main/view/MainFragment.kt
@@ -4,16 +4,13 @@ import android.content.pm.ActivityInfo
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import com.devcamp.network.MatchServiceImpl
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
